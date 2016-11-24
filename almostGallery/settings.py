f3694f7c2f258512cc7584b395ae2bc2b6e97b94
Sverige <immorality7@gmail.com>
2016-11-24 13:26:18 +0100

    fixing static root

--- almostGallery/settings.py
+++ almostGallery/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_ROOT, ]
 MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/'
